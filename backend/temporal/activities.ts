55127feb67940bee903e84acdf1aeecf111f734d
kourjot <pkour6188@gmail.com>
2025-06-13 19:38:31 +0530

    adding logic if user is already exists

--- backend/temporal/activities.ts
+++ backend/temporal/activities.ts
@@ -5,7 +5,7 @@ export async function saveToDatabase(data: any): Promise<void> {
 
 export async function sendToCrudCrud(data: any): Promise<void> {
   try {
-    const apiBase = "https://crudcrud.com/api/9e40e8f2835a4eb3b78053f3faf7ccdb/profile";
+    const apiBase = "https://crudcrud.com/api/2a7e4a1b19684888a78c1390d32fea0b/profile";
 
 
     const getResponse = await fetch(apiBase);
@@ -40,7 +40,13 @@ export async function sendToCrudCrud(data: any): Promise<void> {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
       });
-
+      if(createResponse.ok){
+        const res=await createResponse.json()
+        console.log("New connection",res)
+      }else{
+        const errorText = await createResponse.text();
+        console.log("Create fail",errorText)
+      }
       const result = await createResponse.json();
       console.log("New user created:", result);
     }
